25ec5001b2e3d17a601785f2c39ff7e32b77bc8b
gongas <gddal@iscte-iul.pt>
2019-10-14 13:37:26 +0100

    Nova funcionalidade na class AlunoPL

--- ESI_01/src/Exercicio1/AlunoPL.java
+++ ESI_01/src/Exercicio1/AlunoPL.java
@@ -10,6 +10,16 @@ public class AlunoPL extends Aluno {
 		
 	}
 	
+	public AlunoPL(Aluno aluno, boolean pL) {
+		super(aluno.getNum_aluno(), aluno.getNome(), aluno.getCurso());
+		this.pL = pL;
+		
+	}
+	
+	public boolean getPL() {
+		return pL;
+	}
+	
 	public void mudar(AlunoPL aluno) {
 		this.pL = !pL;
 	}
